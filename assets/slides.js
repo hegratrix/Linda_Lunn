a8651069bd60840683af279df4eadd58efc79b0b
hegratrix <gratrixmomma@yahoo.com>
2018-09-08 02:37:26 -0700

    around town page

---
+++ assets/slides.js
@@ -0,0 +1,89 @@
+let slides = [
+    {"image": "./assets/images/first_day.jpg",
+        "heading": "Back to School with my Zen",
+        "p": "Dorothy McElhinney MS, just one of our many Gold Ribbon Schools."
+    },
+
+    {"image": "./assets/images/curran.jpg",
+        "heading": "",
+        "p": ""
+    },
+
+    {"image": "./assets/images/bookfair2.jpg",
+        "heading": "",
+        "p": ""
+    },
+
+
+    {"image": "./assets/images/bookfair.jpg",
+        "heading": "",
+        "p": ""
+    },
+
+
+    {"image": "./assets/images/vmhs.jpg",
+        "heading": "",
+        "p": ""
+    },
+
+    {"image": "./assets/images/vmhs3.JPEG",
+        "heading": "",
+        "p": ""
+    },
+
+    {"image": "./assets/images/tovashal.jpg",
+        "heading": "",
+        "p": ""
+    },
+
+    {"image": "./assets/images/mmhs_anniv.jpg",
+        "heading": "",
+        "p": ""
+    },
+
+
+    {"image": "./assets/images/band_booster.jpg",
+        "heading": "",
+        "p": ""
+    },
+
+
+    {"image": "./assets/images/vmhs2.jpg",
+        "heading": "",
+        "p": ""
+    },
+
+    {"image": "./assets/images/vmhs4.JPEG",
+        "heading": "",
+        "p": ""
+    },
+
+    {"image": "./assets/images/district.jpg",
+        "heading": "",
+        "p": ""
+    },
+
+    {"image": "./assets/images/district2.jpg",
+        "heading": "",
+        "p": ""
+    },
+
+
+    {"image": "./assets/images/child_develop.jpg",
+        "heading": "",
+        "p": ""
+    },
+
+    {"image": "./assets/images/clinton_keith2.jpg",
+        "heading": "",
+        "p": ""
+    },
+
+
+    {"image": "./assets/images/clinton_keith.jpg",
+        "heading": "",
+        "p": ""
+    },
+
+
+]
