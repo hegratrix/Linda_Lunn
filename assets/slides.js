0489319913f49369d3cf24efbadddb0dda298617
hegratrix <gratrixmomma@yahoo.com>
2018-09-09 19:24:20 -0700

    media query, dropdown menu

--- assets/slides.js
+++ assets/slides.js
@@ -5,84 +5,84 @@ let slides = [
     },
 
     {"image": "./assets/images/curran.jpg",
-        "heading": "",
-        "p": ""
+        "heading": "E Hale Curran",
+        "p": "Firday Flag and Coffee with the Principal"
     },
 
     {"image": "./assets/images/bookfair2.jpg",
-        "heading": "",
-        "p": ""
+        "heading": "Rail Ranch ES Enchanted Forest Book Fair",
+        "p": "Enjoying the book fair with one of Rail's classes"
     },
 
 
     {"image": "./assets/images/bookfair.jpg",
-        "heading": "",
-        "p": ""
+        "heading": "Rail Ranch Elementary's Book Fair",
+        "p": "with awesome parent/PTA helpers Amber Peterson and Sara Anderson"
     },
 
 
     {"image": "./assets/images/vmhs.jpg",
-        "heading": "",
-        "p": ""
+        "heading": "Vista Murrieta HS's first home game",
+        "p": "Celebrating the win with our Golden Broncos superfans, Go Broncos!"
+    },
+    
+
+    {"image": "./assets/images/vmhs2.jpg",
+        "heading": "Vista Murrieta HS's first home game",
+        "p": "Endzone fun with Erin Nathan.  Vista took the win!"
     },
 
     {"image": "./assets/images/vmhs3.JPEG",
-        "heading": "",
+        "heading": "Vista Murrieta HS",
         "p": ""
     },
 
     {"image": "./assets/images/tovashal.jpg",
-        "heading": "",
-        "p": ""
+        "heading": "Tovashal ES Willy Wonka Book Fair",
+        "p": "At the book fair with Nikki Harshaw and Katie Adams, they even had a chocolate waterfall!"
     },
 
     {"image": "./assets/images/mmhs_anniv.jpg",
-        "heading": "",
-        "p": ""
+        "heading": "Murrieta Mesa HS's 10th Anniversary",
+        "p": "Jim, Zen and Ken Dickson celebrate in the endzone."
     },
 
 
     {"image": "./assets/images/band_booster.jpg",
-        "heading": "",
-        "p": ""
-    },
-
-
-    {"image": "./assets/images/vmhs2.jpg",
-        "heading": "",
-        "p": ""
+        "heading": "Dorothy McElhinney Band Booster",
+        "p": "Coach Rob Stout supervises the recycling fundraiser where the color guard and drumline sorted $800 worth of plastic and cans.  Recycle, reuse, renew!"
     },
 
     {"image": "./assets/images/vmhs4.JPEG",
-        "heading": "",
+        "heading": "Vista Murrieta HS",
         "p": ""
     },
 
     {"image": "./assets/images/district.jpg",
-        "heading": "",
-        "p": ""
+        "heading": "Murrieta Valley USD District Offices",
+        "p": "With Carol Hernandez,  VAPA Coordinator extraordinaire"
     },
 
     {"image": "./assets/images/district2.jpg",
-        "heading": "",
-        "p": ""
+        "heading": "Educational Services Retreat",
+        "p": "with Jay Brown, Laura Gonzalez and Mary Walters"
     },
 
 
     {"image": "./assets/images/child_develop.jpg",
-        "heading": "",
-        "p": ""
+        "heading": "Child Development Center",
+        "p": "Kathleen Israelsen and Staff"
     },
 
     {"image": "./assets/images/clinton_keith2.jpg",
-        "heading": "",
-        "p": ""
+        "heading": "Clinton Keith Grand Opening",
+        "p": "Local Boy Scout Color Guard presents the flags"
     },
 
 
     {"image": "./assets/images/clinton_keith.jpg",
-        "heading": "",
-        "p": ""
+        "heading": "Clinton Keith Grand Opening",
+        "p": "Celebrating with local moms Hope Gratrix and Gail Vergara"
     },
 
 
